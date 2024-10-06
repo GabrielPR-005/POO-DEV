9857db8679be2d5892208a191cd5ef4ca365166c
Ricardo <00119110450@pq.uenf.br>
2024-10-05 21:17:06 -0300

    Implementação do redirecionamento para a página home

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ try {
   <body>
 
     <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="home.php">
         <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
       </a>
