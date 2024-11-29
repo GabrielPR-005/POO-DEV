d28dc3f58b2b3c3e3d2d6bc4ed3c1dd6a33afbc1
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2024-11-29 16:38:55 -0300

    Update Usuario.php

--- classes/Usuario.php
+++ classes/Usuario.php
@@ -2,16 +2,20 @@
 require_once "DatabaseConnection.php";
 
 class Usuario {
-    private $email;
-    private $conexao;
+    protected $id;
+    protected $email;
+    protected $tipo;
+    protected $senha;
+    protected $conexao;
 
     public function __construct($email) {
         $this->email = $email;
         $this->conexao = (new DatabaseConnection())->getConexao();
+        $this->loadByEmail();
     }
 
-    public function getId() {
-        $query = "SELECT id FROM usuarios WHERE email = ?";
+    protected function loadByEmail() {
+        $query = "SELECT * FROM usuarios WHERE email = ?";
         $stmt = $this->conexao->prepare($query);
         $stmt->bind_param("s", $this->email);
         $stmt->execute();
@@ -19,17 +23,49 @@ class Usuario {
 
         if ($result->num_rows > 0) {
             $user = $result->fetch_assoc();
-            return $user['id'];
+            $this->id = $user['id'];
+            $this->tipo = $user['tipo'];
+            $this->senha = $user['senha'];
+        } else {
+            throw new Exception("Usuário não encontrado.");
         }
-        throw new Exception("Usuário não encontrado.");
+    }
+
+    public function getTipo() {
+        return $this->tipo;
+    }    
+
+    public function getId() {
+        return $this->id;
     }
 
     public function autenticar($senha) {
-        $query = "SELECT * FROM usuarios WHERE email = ? AND senha = ?";
+        return $senha === $this->senha;
+    }
+}
+
+class UsuarioById extends Usuario {
+    public function __construct($id) {
+        $this->id = $id;
+        $this->conexao = (new DatabaseConnection())->getConexao();
+        $this->loadById();
+    }
+
+    protected function loadById() {
+        $query = "SELECT * FROM usuarios WHERE id = ?";
         $stmt = $this->conexao->prepare($query);
-        $stmt->bind_param("ss", $this->email, $senha);
+        $stmt->bind_param("i", $this->id);
         $stmt->execute();
-        return $stmt->get_result()->num_rows > 0;
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            $this->email = $user['email'];
+            $this->tipo = $user['tipo'];
+            $this->senha = $user['senha'];
+        } else {
+            throw new Exception("Usuário não encontrado.");
+        }
     }
 }
 ?>
