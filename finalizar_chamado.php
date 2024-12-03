90cec093f1611c37049d77b6cc550cce8e850118
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2024-12-03 17:06:37 -0300

    Update finalizar_chamado.php

--- finalizar_chamado.php
+++ finalizar_chamado.php
@@ -14,14 +14,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         $id_chamado = $_POST['id_chamado'];
 
-        // Obter o chamado pelo ID
+
         $chamado = new Chamado($id_chamado);
 
-        // Obter o ID do técnico a partir da sessão
+
         $usuario = new Usuario(SessionManager::getEmail());
         $tecnicoId = $usuario->getId();
 
-        // Chamar o método para finalizar o chamado
+
         $chamado->finalizarChamado($tecnicoId);
 
         header("Location: consultar_chamados_tecnico.php");
